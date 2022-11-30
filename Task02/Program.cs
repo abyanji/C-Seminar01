4365a601f412ec7ada4b6f18ec3c3c8a9f0b65a3
abyanji <a.byanjil99@gmail.com>
2022-11-30 15:43:23 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Напишите два числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a/b == b)
+{
+    Console.WriteLine($"Число {a} является квадратом числа {b}");
+}
+else
+{
+    Console.WriteLine($"Число {a}  НЕ является квадратом числа {b}");
+}
